e941efc008b6fd567699e55b32d0d8ac085a50e0
OFA <omerfaruk.aksoy@hotmail.com>
2022-02-28 18:17:23 +0300

    Initialized project on Xcode

---
+++ Birds of Istanbul/Birds of Istanbul/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Birds of Istanbul
+//
+//  Created by Omer Faruk Aksoy on 28.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
