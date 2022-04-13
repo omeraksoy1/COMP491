94d76cb083dffe8a4a4371f7fd364205beaad69a
abond100 <abond19@ku.edu.tr>
2022-04-13 23:42:08 +0300

    Undoing pods commit

--- Pods/Target Support Files/ActivityIndicatorView/ActivityIndicatorView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ActivityIndicatorView : NSObject
-@end
-@implementation PodsDummy_ActivityIndicatorView
-@end
--- Pods/Target Support Files/Pods-BirdsOfIstanbul/Pods-BirdsOfIstanbul-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BirdsOfIstanbul : NSObject
-@end
-@implementation PodsDummy_Pods_BirdsOfIstanbul
-@end
